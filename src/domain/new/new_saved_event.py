14929e78f0755b3d2c8b321b42c5619e8305aecb
DeejayRevok <seryi_one@hotmail.com>
2023-07-10 01:05:19 +0200

    Remove declarative events definition

--- src/domain/new/new_saved_event.py
+++ src/domain/new/new_saved_event.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass, field
 from typing import Optional, List
 
-from bus_station.event_terminal.event import Event
-
 
 @dataclass(frozen=True)
-class NewSavedEvent(Event):
+class NewSavedEvent:
     title: str
     url: str
     source: str
@@ -13,7 +11,3 @@ class NewSavedEvent(Event):
     language: str
     entities: List[dict] = field(default_factory=list)
     sentiment: Optional[float] = None
-
-    @classmethod
-    def passenger_name(cls) -> str:
-        return "event.new_saved"
