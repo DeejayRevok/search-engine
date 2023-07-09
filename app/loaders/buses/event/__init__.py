c395a3b2d7d5c4815fe2931715b60d645d786ce6
DeejayRevok <seryi_one@hotmail.com>
2023-07-10 01:04:17 +0200

    Add passenger mapping

--- app/loaders/buses/event/__init__.py
+++ app/loaders/buses/event/__init__.py
@@ -1,5 +1,7 @@
 from app.loaders.buses.event.kombu_event_bus_loader import load as load_kombu_event_bus
+from app.loaders.buses.event.passenger_loader import load as load_event_passengers
 
 
 def load() -> None:
+    load_event_passengers()
     load_kombu_event_bus()
